85341cc716fdfac1c96e1215200ce3d73c41b016
yasith <it19966922@my.sliit.lk>
2022-11-17 00:17:34 +0530

    implement test cases

--- __test__/message.test.js
+++ __test__/message.test.js
@@ -3,6 +3,16 @@ const {saveMessage} = require('../controllers/message.controller');
 // Path: __test__\message.test.js
 // Compare this snippet from controllers\message.controller.js:
 
-it('should save message', async () => {
+const invalidRequest = {
+    body: {
+        message1: "Hello World"
+    },
+    headers: {
+        signature: "d54f5rgfd5454g"
+    }
+};
+
+// check with invalid payload
+it('should return message with 401 status code', async () => {
 
 })
