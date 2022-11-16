906af01b1fd54c476989ccd987ec3b5a5fc715de
yasith <it19966922@my.sliit.lk>
2022-11-16 23:56:38 +0530

    implement test cases

--- __test__/message.test.js
+++ __test__/message.test.js
@@ -3,3 +3,6 @@ const {saveMessage} = require('../controllers/message.controller');
 // Path: __test__\message.test.js
 // Compare this snippet from controllers\message.controller.js:
 
+it('should save message', async () => {
+
+})
