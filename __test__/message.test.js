2e00a955ce2398e35feccbdb42e06150b9e70648
yasith <it19966922@my.sliit.lk>
2022-11-17 00:23:14 +0530

    implement test cases

--- __test__/message.test.js
+++ __test__/message.test.js
@@ -12,7 +12,35 @@ const invalidRequest = {
     }
 };
 
+const validRequest1 = {
+    body: {
+        message: "Hello World"
+    },
+    headers: {
+        signature: "d54f5rgfd5454g"
+    }
+}
+
+const validRequest2 = {
+    body: {
+        message: "Hello World"
+    }
+}
+
 // check with invalid payload
 it('should return message with 401 status code', async () => {
+    const response = await request(app).post('/message').send(invalidRequest.body).set(invalidRequest.headers);
+    expect(response.statusCode).toBe(401);
+})
 
+// check with invalid signature payload
+it('should return message with 200 status code', async () => {
+    const response = await request(app).post('/message').send(validRequest1.body).set(validRequest1.headers);
+    expect(response.statusCode).toBe(200);
+})
+
+// check with without signature  payload
+it('should return message with 401 status code', async () => {
+    const response = await request(app).post('/message').send(validRequest2.body).set(validRequest2.headers);
+    expect(response.statusCode).toBe(401);
 })
